c7c67347bf58ad08292e825bdc1bfbf2e4ad0a4b
rikgj <44588337+rikgj@users.noreply.github.com>
2020-12-13 17:01:00 +0100

    Some rewrite and added comments to 30-39

--- 30-39/p_30.py
+++ 30-39/p_30.py
@@ -1,5 +1,6 @@
 '''
-Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+Surprisingly there are only three numbers that can be written as
+the sum of fourth powers of their digits:
 
 1634 = 14 + 64 + 34 + 44
 8208 = 84 + 24 + 04 + 84
@@ -10,36 +11,16 @@ The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
 Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 '''
-import timeit
 
-def powersums():
-    num = 2
-    power = 5
-    winners = []
+num = 2
+power = 5
+winners = []
+# find the sum of the single digits by the power of len(num)
+while num < 355000:
+    # check if the sum of the digits**5 in num == num
+    if(sum([int(x)**power for x in str(num)]) == num):
+        winners.append(num)
+    num+=1
 
-    # find the sum of the single digits by the power of len(num)
-    while num < 355000:
-        sum = 0
-        num_str = str(num)
-        digits = list(num_str)
-
-        for dig in digits:
-            sum+= int(dig)**power
-
-        if(sum == num):
-            winners.append(num)
-
-        num+=1
-
-    # present the winners
-    print("Number of winners: {}\nThe winners:\n{}".format(len(winners), winners))
-    sum = 0
-    for winner in winners:
-        sum+=winner
-    print(sum)
-
-
-powersums()
-
-# execution_time = timeit.timeit(powersums, number=10)
-# print(execution_time)
+# present the winners
+print("The winners: {}\nThe sum of theese: {}".format(winners, sum(winners)))
